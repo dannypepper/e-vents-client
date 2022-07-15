55c42722ccf60ee064c72fe3cf845d3cf1b729d3
dannypepper <bors.dani@gmail.com>
2022-07-15 22:41:18 +0200

    WIP style form

--- src/templates/CreateEvent.vue
+++ src/templates/CreateEvent.vue
@@ -1,86 +1,89 @@
 <template>
   <Layout>
-    <v-col cols="12">
-      <h2 class="mb-4">Create Event Page</h2>
-      <v-card>
-        <v-form
-          ref="form"
-          v-model="valid"
-          lazy-validation
-        >
-          <v-text-field
-            v-model="title"
-            :counter="10"
-            :rules="titleRules"
-            label="Title"
-            required
-            class="col-12"
+    <v-row>
+      <v-col cols="12">
+        <h2 class="mb-4">Create Event Page</h2>
+        <v-card class="py-2 px-2">
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
           >
-          </v-text-field>
-
-          <v-text-field
-            v-model="description"
-            label="Description"
-            required
-            clear-icon
-            class="col-12"
-          ></v-text-field>
-
-          <v-text-field
-            v-model="price"
-            :rules="priceRules"
-            label="Price"
-            required
-            class="col-12"
-          >
-          </v-text-field>
-
-          <v-select
-            v-model="select"
-            label="Select categories"
-            required
-            class="col-12"
-          >
-          </v-select>
-
-          <v-date-picker
-            v-model="date"
-            :rules="dateRules"
-            label="Date"
-            color="green"
-            required
-            class="col-6"
-          >
-          </v-date-picker>
-
-          <v-time-picker
-            v-model="timeStep"
-            color="green"
-            format="24hr"
-            :allowed-minutes="allowedStep"
-            class="col-6"
-          >
-          </v-time-picker>
-
-          <v-col cols="12">
-            <v-btn
-              color="success"
-              class="mr-4"
+            <v-text-field
+              v-model="title"
+              :rules="titleRules"
+              label="Title"
+              filled
+              required
             >
-              Validate
-            </v-btn>
+            </v-text-field>
 
-            <v-btn
-              color="error"
-              class="mr-4"
-              @click="reset"
+            <v-textarea
+              v-model="description"
+              auto-grow
+              filled
+              label="Description"
             >
-              Reset Form
-            </v-btn>
-          </v-col>
-        </v-form>
-      </v-card>
-    </v-col>
+            </v-textarea>
+
+            <v-text-field
+              v-model="price"
+              :rules="priceRules"
+              label="Price"
+              filled
+              required
+            >
+            </v-text-field>
+
+            <v-select
+              v-model="select"
+              label="Select categories"
+              filled
+              required
+            >
+            </v-select>
+
+            <v-date-picker
+              v-model="date"
+              :rules="dateRules"
+              label="Date"
+              class="col-6"
+              color="green"
+              dark
+              required
+            >
+            </v-date-picker>
+
+            <v-time-picker
+              v-model="timeStep"
+              color="green"
+              format="24hr"
+              :allowed-minutes="allowedStep"
+              class="col-6"
+              dark
+            >
+            </v-time-picker>
+
+            <v-col cols="12" style="display: flex; justify-content: space-between;">
+              <v-btn
+                class="mr-4"
+                color="error"
+                @click="clearForm"
+              >
+                Clear Form
+              </v-btn>
+
+              <v-btn
+                color="success"
+                @click="submitForm"
+              >
+                Validate
+              </v-btn>
+            </v-col>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
   </Layout>
 </template>
 
@@ -94,22 +97,27 @@ export default {
     ).toISOString().substr(0, 10),
   }),
   methods: {
+    valid: true,
     allowedStep: (m) => m % 15 === 0,
+    clearForm() {
+      this.$refs.form.reset();
+    },
     encode(data) {
       return Object.keys(data)
         .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
         .join('&');
     },
-    handleSubmit(e) {
-      fetch('/', {
+    submitForm(e) {
+      this.$refs.form.validate();
+      fetch('http://localhost:1337/api/events', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: this.encode({
-          'form-name': e.target.getAttribute('name'),
+          formData: e.target.getAttribute('name'),
           ...this.formData,
         }),
       })
-        .then(() => this.$router.push('/success'))
+        .then(() => this.$router.push('/'))
         .catch((error) => alert(error));
     },
   },
